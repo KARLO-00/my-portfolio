7bba1c5606f5cb60f22a2f05a022f4d5fe7150ab
John Carlo <johncarlobalcita.business@gmail.com>
2026-06-20 09:19:51 +0800

    Move techstack and experience
    Instead of displaying in about section, move it to project preview section

--- src/sections/ProjectsPreview.jsx
+++ src/sections/ProjectsPreview.jsx
@@ -1,6 +1,39 @@
 import { Link } from "react-router-dom";
 
 export default function ProjectsPreview({ darkMode }) {
+    const skills = [
+        {
+            "category": 'Frontend',
+            'techlist': [
+                "JavaScript",
+                "jQuery",
+                "React",
+                "Tailwind CSS",
+                "Bootstrap"]
+        },
+        {
+            'category': 'Backend',
+            'techlist': [
+                "PHP",
+                "Laravel",
+                "PostgreSQL",
+                "MySQL",
+                "Node.js"
+            ]
+        },
+        {
+            'category': 'Developer Tools',
+            'techlist': [
+                "Git",
+                "Github",
+                "VS Code",
+                "Teams",
+                "Trello",
+                "draw.io"
+            ]
+        }
+    ]
+
     const projects = [
         {
             title: "Production Monitoring System",
@@ -31,79 +64,138 @@ export default function ProjectsPreview({ darkMode }) {
     return (
         <section
             id="projects"
-            className="py-24 flex items-center justify-center px-6"
+            className="min-h-screen px-6 lg:px-80 py-20 flex flex-col lg:flex-row gap-5"
         >
-            <div className="max-w-6xl mx-auto">
+            <div className="lg:w-3/4">
+                {/* Tech Stack */}
+                <div className="w-full lg:w-full">
+                    <div className={`border  rounded-xl p-6
+                            ${darkMode ? "bg-gray-950 border-gray-800" : "bg-white border-gray-200"}
+                        `}>
 
-                {/* Header */}
-
-
-                <div className="flex items-center justify-between mb-4">
-                    <h1 className="text-3xl md:text-4xl font-bold mb-10">
-                        Projects
-                    </h1>
-
-                    <Link
-                        to="/projects"
-                        className="text-sm text-blue-400 hover:text-blue-300 transition"
-                    >
-                        View All
-                    </Link>
-                </div>
-
-                {/* Grid */}
-                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-
-                    {projects.map((project, index) => (
-                        <div
-                            key={index}
-                            className={`border rounded-xl p-6 hover:border-blue-400 transition
-                                ${darkMode ? "bg-gray-950 border-gray-800" : "bg-white border-gray-200"}
-                                `}
-                        >
-                            <h2 className="text-xl font-medium mb-2">
-                                {project.title}
+                        <div className="flex items-center justify-between mb-4">
+                            <h2 className="text-xl font-medium">
+                                Tech Stack
                             </h2>
 
-                            <p className="text-gray-400 text-sm mb-4">
-                                {project.description}
-                            </p>
-
-                            {/* Tech stack */}
-                            <div className="flex flex-wrap gap-2 mb-4">
-                                {project.tech.map((t) => (
-                                    <span
-                                        key={t}
-                                        className={`px-2 py-1 text-xs rounded-md
-                                            ${darkMode ? "bg-gray-800 text-gray-300" : "bg-gray-200 text-gray-700"}
-                                            `}
-                                    >
-                                        {t}
-                                    </span>
-                                ))}
-                            </div>
-
-                            {/* Links */}
-                            <div className="flex gap-4 text-sm">
-                                <a
-                                    href={project.github}
-                                    className="text-blue-400 hover:text-blue-300"
-                                >
-                                    GitHub
-                                </a>
-
-                                <a
-                                    href={project.live}
-                                    className="text-blue-400 hover:text-blue-300"
-                                >
-                                    Live Demo
-                                </a>
-                            </div>
+                            <Link
+                                to="/experience"
+                                className="text-sm text-blue-400 hover:text-blue-300 transition"
+                            >
+                                View All
+                            </Link>
                         </div>
-                    ))}
 
+                        <div className="space-y-4">
+
+                            {skills.map((item) => (
+                                <div key={item.category}>
+                                    <h3 className={`"text-sm mb-2" ${darkMode ? "text-gray-400" : "text-gray-900"}`}>
+                                        {item.category}
+                                    </h3>
+
+                                    <div className="flex flex-wrap gap-2">
+                                        {item.techlist.map((tech) => (
+                                            <span
+                                                key={tech}
+                                                className={`px-3 py-1 text-sm rounded-lg 
+                                                    ${darkMode ? "bg-gray-800" : "bg-gray-200"}
+                                                `}
+                                            >
+                                                {tech}
+                                            </span>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
+
+                        </div>
+                    </div>
+                </div>
+
+                <br />
+
+                {/* Projects */}
+                <div className="w-full lg:w-full">
+                    <div className={`border  rounded-xl p-6
+                            ${darkMode ? "bg-gray-950 border-gray-800" : "bg-white border-gray-200"}
+                        `}>
+
+                        <div className="flex items-center justify-between mb-4">
+                            <h2 className="text-xl font-medium">
+                                Tech Stack
+                            </h2>
+
+                            <Link
+                                to="/experience"
+                                className="text-sm text-blue-400 hover:text-blue-300 transition"
+                            >
+                                View All
+                            </Link>
+                        </div>
+
+                        <div className="space-y-4">
+
+                            {skills.map((item) => (
+                                <div key={item.category}>
+                                    <h3 className={`"text-sm mb-2" ${darkMode ? "text-gray-400" : "text-gray-900"}`}>
+                                        {item.category}
+                                    </h3>
+
+                                    <div className="flex flex-wrap gap-2">
+                                        {item.techlist.map((tech) => (
+                                            <span
+                                                key={tech}
+                                                className={`px-3 py-1 text-sm rounded-lg 
+                                                    ${darkMode ? "bg-gray-800" : "bg-gray-200"}
+                                                `}
+                                            >
+                                                {tech}
+                                            </span>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
+
+                        </div>
+                    </div>
                 </div>
             </div>
-        </section>
+
+            <div className="lg:w-1/4 lg:w-96 space-y-6">
+
+                {/* Experience */}
+                <div className={`rounded-xl p-6
+                    ${darkMode ? "bg-gray-950 border border-gray-800" : "bg-white border border-gray-200"}
+                    `}>
+
+                    <div className="flex items-center justify-between mb-4">
+                        <h2 className="text-xl font-medium">
+                            Experience
+                        </h2>
+
+                    </div>
+                    <div className="space-y-4">
+
+                        {/* Experience */}
+                        <div className="border-l border-gray-700 pl-4">
+                            <h3 className="text-white font-medium">
+                                Hello, World! 👋
+                            </h3>
+
+                            <p className="text-sm text-gray-400">
+                                Wrote my first line of code
+                            </p>
+
+                            <p className="text-xs text-gray-500 mt-1">
+                                2021 - 2023
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </section >
     );
 }
